8390f61566628bc95132a9fb69e193de051b8ee0
ItaruOriginal <italocoelho45@gmail.com>
2023-05-22 19:13:09 -0300

    Ajustes 2

--- receitas/views.py
+++ receitas/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return render(request,'home.html')
+
+
+def contato(request):
+    return HttpResponse("contato")
+
+def sobre(request):
+    return HttpResponse("sobre")
+
+urlpatterns =[
+    path('admin/', admin.site.urls),
+    path('',home),
+    path('contato/',contato),
+    path('sobre/',sobre), 
+    ]
